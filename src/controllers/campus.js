63bb8dd9fc96c313e49407961b2f5b39d1be85d0
ink0rr <tomflynnbeltsazar@gmail.com>
2022-11-30 20:36:09 +0700

    Add campus update and delete methods

--- src/controllers/campus.js
+++ src/controllers/campus.js
@@ -1,3 +1,4 @@
+import AuthorizationError from "../commons/exceptions/AuthorizationError.js";
 import NotFoundError from "../commons/exceptions/NotFoundError.js";
 import { Campus } from "../models/Campus.js";
 
@@ -51,6 +52,63 @@ export async function addCampus(req, res, next) {
       admin: userId,
     });
     res.status(201).json(campus);
+  } catch (err) {
+    next(err);
+  }
+}
+
+/** @type{import("express").RequestHandler} */
+export async function updateCampus(req, res, next) {
+  try {
+    const { userId } = res.locals.token;
+    const { campusId } = req.params;
+    const {
+      name,
+      address,
+      description,
+      accreditation,
+      status,
+      faculties,
+      links,
+      imageUrl,
+    } = req.body;
+    const campus = await Campus.findById(campusId);
+    if (!campus) {
+      throw new NotFoundError("Campus not found");
+    }
+    if (campus.admin.toString() !== userId) {
+      throw new AuthorizationError("Restricted resource");
+    }
+    await campus.updateOne({
+      name,
+      address,
+      description,
+      accreditation,
+      status,
+      faculties,
+      links,
+      imageUrl,
+    });
+    res.status(200).json({ message: "success" });
+  } catch (err) {
+    next(err);
+  }
+}
+
+/** @type{import("express").RequestHandler} */
+export async function deleteCampus(req, res, next) {
+  try {
+    const { userId } = res.locals.token;
+    const { campusId } = req.params;
+    const campus = await Campus.findById(campusId);
+    if (!campus) {
+      throw new NotFoundError("Campus not found");
+    }
+    if (campus.admin.toString() !== userId) {
+      throw new AuthorizationError("Restricted resource");
+    }
+    await campus.deleteOne();
+    res.status(200).json(campus);
   } catch (err) {
     next(err);
   }
--- src/routes/campus.js
+++ src/routes/campus.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
   addCampus,
+  deleteCampus,
   getCampusById,
   getCampuses,
+  updateCampus,
 } from "../controllers/campus.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { threads } from "./threads.js";
@@ -14,5 +16,7 @@ campus
   .get("/:campusId", getCampusById)
 
   .post("/", authMiddleware, addCampus)
+  .put("/:campusId", authMiddleware, updateCampus)
+  .delete("/:campusId", authMiddleware, deleteCampus)
 
   .use("/:campusId/threads", threads);
